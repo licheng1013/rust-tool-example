bd5f7f22090783068fa3f5c33cb2bf56cc8b76f7
licheng <2501093733@qq.com>
2023-10-09 17:17:10 +0800

    增加中间件处理功能!

--- salvo-web/src/plugin/config.rs
+++ salvo-web/src/plugin/config.rs
@@ -1,6 +1,11 @@
 use salvo::prelude::*;
 
 #[handler]
-pub async fn plugin(_req: &mut Request, _depot: &mut Depot,res: &mut Response) {
-    res.render("Hello world");
+pub async fn plugin(_req: &mut Request, _depot: &mut Depot,res: &mut Response,ctrl: &mut FlowCtrl) {
+    let path = _req.uri().path();
+    println!("plugin path: {}", path);
+    if path == "/admin/list" {
+        res.render("Hello world");
+        ctrl.cease(); // 跳过所有处理
+    }
 }
