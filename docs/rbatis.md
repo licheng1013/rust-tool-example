53a91cdb3bf6b77a766eb9cd538b6eb3e018fc5c
licheng <2501093733@qq.com>
2024-04-17 18:19:59 +0800

    修改 Rbatis 文档!

--- docs/rbatis.md
+++ docs/rbatis.md
@@ -1,7 +1,19 @@
 # Rbatis
 
+## 官网
+- https://github.com/rbatis/rbatis
+
 ## 介绍
 
 - 支持暂时有限
 - 注意: html标签是根据方法前缀来识别创建相应的标签，如select_xx -> select标签,默认也是select标签。
-- ![](images/doc2.png)
+- ![](rbatis/doc2.png)
+
+
+## 字段提示
+
+- 插件版本要求: `2024.1.3+`
+- ![img.png](rbatis/img.png)
+- 支持以下方法的类字段补全:
+- "select_by_column", "delete_by_column", "update_by_column", "select_in_column", "delete_in_column"
+- 使用方式: 输入 "" 后在中输入类名的开头如图上: "U" 即可触发补全
---
+++ rbatis-example/src/main.rs
@@ -0,0 +1,53 @@
+use rbatis::{crud, impl_select, RBatis};
+use rbatis::rbatis_codegen::ops::AsProxy;
+use rbatis::rbdc::datetime::DateTime;
+use serde_json::json;
+
+#[derive(serde::Serialize, serde::Deserialize, Clone)]
+pub struct User {
+    /// 用户id
+    pub id: Option<i32>,
+    /// 用户昵称
+    pub name: Option<String>,
+    /// 手机号
+    pub tel: Option<String>,
+    /// 账号金额: 分
+    pub money: Option<i64>,
+    /// 创建时间
+    pub create_time: Option<DateTime>,
+}
+
+//@Rbatis(User)
+impl_select!(User{select_by_id(val:&str) -> Option => "`where id = #{val} limit 1`"},TABLE_NAME);
+
+
+crud!(User {},"t_user"); // impl_insert!($table {}) + impl_select!($table {}) + impl_update!($table {}) + impl_delete!($table {});
+
+#[tokio::main]
+pub async fn main() {
+    let rb = RBatis::new();
+    rb.init(rbdc_mysql::driver::MysqlDriver {}, "mysql://root:root@192.168.101.90/t_gorm").unwrap();
+
+    let table = User {
+        id: None,
+        name: Some("2".into()),
+        tel: Some("2".into()),
+        money: Some(11),
+        create_time: Some(DateTime::now()),
+    };
+    let tables = [table.clone(), {
+        table.clone()
+    }];
+
+    let data = User::insert(&rb, &table).await;
+    println!("insert = {}", json!(data));
+    let data = User::insert_batch(&rb, &tables, tables.len().u64()).await;
+    println!("insert_batch = {}", json!(data));
+    let data = User::select_by_column(&rb, "name", 2).await;
+    println!("select_in_column = {}", json!(data));
+    let data = User::delete_by_column(&rb, "", 33).await;
+    println!("delete_in_column = {}", json!(data));
+    let v = User::select_by_column(&rb,"","");
+    User::select_by_column(&rb,"U","");
+}
+
